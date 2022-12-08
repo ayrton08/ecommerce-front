1b27bf16acefdeaa036e36059c80a197c288d9e5
Ayrton <ayrtonjuarez90@gmail.com>
2022-12-07 21:06:19 -0300

    deploy error next.config.js

--- next.config.js
+++ next.config.js
@@ -12,19 +12,16 @@ const nextConfig = withSvgr({
       {
         protocol: "https",
         hostname: "dl.airtable.com",
-        port: "",
         pathname: "/.attachments/**",
       },
       {
         protocol: "https",
         hostname: "img.freepik.com",
-        port: "",
         pathname: "/vector-gratis/**",
       },
       {
         protocol: "https",
         hostname: "cdn-icons-png.flaticon.com",
-        port: "",
         pathname: "/*/**",
       },
     ],
