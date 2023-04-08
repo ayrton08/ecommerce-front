30f7082b85eb9a3258dc4575c659c5e2a219419d
Ayrton <ayrtonjuarez90@gmail.com>
2023-04-08 13:53:41 -0300

    fix: env var Next_URl

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import NextAuth from 'next-auth';
+import NextAuth, { SessionStrategy } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 import Credentials from 'next-auth/providers/credentials';
