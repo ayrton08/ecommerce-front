77cdd198f6410e79fa3fdfee05551c984ed94d1b
Ayrton <ayrtonjuarez90@gmail.com>
2023-04-12 11:17:49 -0300

    feat: middleware global

--- middleware.ts
+++ middleware.ts
@@ -2,14 +2,14 @@ import { getToken } from 'next-auth/jwt';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(req: NextRequest) {
-  // const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-  // console.log(req.nextUrl);
-  // if (req.nextUrl.pathname.startsWith('/signin')) {
-  //   if (session) {
-  //     console.log({ session });
-  //     return NextResponse.rewrite(new URL('/', req.url));
-  //   }
-  // }
+  const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  console.log(req.nextUrl);
+  if (req.nextUrl.pathname.startsWith('/signin')) {
+    if (session) {
+      console.log({ session });
+      return NextResponse.rewrite(new URL('/', req.url));
+    }
+  }
   // if (!session) {
   //   const requestedPage = req.nextUrl.pathname;
   //   const url = req.nextUrl.clone();
